db545d5cdbad9bbf6e860cd079ed39e50c3adb15
Bruno Parga <brunoparga@gmail.com>
2023-10-26 17:43:15 -0300

    Enable test descriptions and multiple tests/file

--- test/test.c
+++ test/test.c
@@ -1,6 +1,5 @@
 #include <err.h>
 #include <fts.h>
-#include <stdio.h>
 #include <string.h>
 
 #include "../utils/read_file.c"
@@ -32,20 +31,21 @@ static inline int is_test(char *path) {
   return !strcmp(extension, ".lye");
 }
 
-static char *build_command(char *filename, char *result) {
+static char *build_command(char *source, char *result) {
   char *lye = "./build/lye";
-  char concat[strlen(lye) + strlen(filename) + 2];
-  concat[0] = '\0';
+  char concat[strlen(lye) + strlen(source) + 6];
+  memset(concat, 0, strlen(concat));
   strcat(concat, lye);
-  strcat(concat, " ");
-  strcat(concat, filename);
+  strcat(concat, " -s \"");
+  strcat(concat, source);
+  strcat(concat, "\"");
   strcpy(result, concat);
   return result;
 }
 
-static char *get_expected(char *test) {
+static char *get_expected(char *line) {
   // Find the comment
-  char *comment = strchr(test, ';');
+  char *comment = strchr(line, ';');
   // Advance to the first space after the semicolon
   comment = strchr(comment, ' ');
   comment++;
@@ -55,6 +55,12 @@ static char *get_expected(char *test) {
   comment[strcspn(comment, "\n")] = '\0';
   // Return the actual expected value
   return ++comment;
+}
+
+static inline char *get_result(char *tmp_file) {
+  char *result = read_file(tmp_file);
+  result[strcspn(result, "\n")] = '\0';
+  return result;
 }
 
 static void print_fail(char *filename, char *expected, char *result) {
@@ -70,32 +76,64 @@ static void print_fail(char *filename, char *expected, char *result) {
   printf("%s", error_msg);
 }
 
-static void run_test(char *test_file, char *tmp_file, int *successes, int *failures) {
+static void run_test(char *test_filename, char *result_file, int *successes,
+                     int *failures) {
   // Call the Lye executable on a given test file
   char command[100];
-  freopen(tmp_file, "w+", stdout);
-  system(build_command(test_file, command));
+  FILE *test_file = fopen(test_filename, "r");
 
-  // Read the expected result from the test file
-  char *test = read_file(test_file);
-  char *expected = get_expected(test);
+  if (test_file == NULL) {
+    err(1, "Error opening test file %s\n", test_filename);
+  }
 
-  // Read the actual result from the temporary file
-  char *result = read_file(tmp_file);
-  result[strcspn(result, "\n")] = '\0';
+  char *line = NULL;
+  size_t length = 0;
+
+  while ((getline(&line, &length, test_file)) != -1) {
+    if (strcmp(line, "\n") == 0) { // Empty lines in the test file
+      continue;
+    }
+    // Read the test from the test line
+    size_t semicolon_position = strcspn(line, ";");
+    char test[semicolon_position];
+    memcpy(test, line, semicolon_position);
+    test[semicolon_position] = '\0';
+
+    // Run the test if the line is not just a comment
+    if (semicolon_position > 0) {
+      freopen(result_file, "w+", stdout);
+
+      // Get the expected result
+      char *expected = get_expected(line);
+
+      // Run the test, leaving the result in the temporary file
+      char *foo = build_command(test, command);
+      system(foo);
+      memset(command, 0, strlen(command));
+
+      // Read the actual result from the temporary file
+      char *result = get_result(result_file);
+
+      // Increment the success or failure count and print failure
+      if (strcmp(result, expected) == 0) {
+        *successes += 1;
+      } else {
+        *failures += 1;
+        print_fail(test_filename, expected, result);
+      }
+    }
+  }
 
   freopen("/dev/tty", "w", stdout);
-
-  if (strcmp(result, expected) == 0) {
-    *successes += 1;
-  } else {
-    *failures += 1;
-    print_fail(test_file, expected, result);
+  // Clean up
+  fclose(test_file);
+  if (line) {
+    free(line);
   }
 }
 
 int main(void) {
-  char *tmp_file = "./test/test.tmp";
+  char *result_file = "./test/test.tmp";
   FTS *test_dir = open_tests_dir();
   FTSENT *subdir;
 
@@ -109,17 +147,20 @@ int main(void) {
 
   while ((subdir = fts_read(test_dir)) != NULL) {
     if (subdir->fts_info == FTS_F && is_test(subdir->fts_path)) {
-      run_test(subdir->fts_path, tmp_file, succ_ptr, fail_ptr);
+      run_test(subdir->fts_path, result_file, succ_ptr, fail_ptr);
     }
   }
 
   int total = successes + failures;
   if (failures == 0) {
-    printf("Ran " ANSI_COLOR_GREEN "%d" ANSI_COLOR_RESET " tests, all passed. Hooray!\n", total);
+    printf("Ran " ANSI_COLOR_GREEN "%d" ANSI_COLOR_RESET
+           " tests, all passed. Hooray!\n",
+           total);
   } else {
-    printf("Ran %d tests, " ANSI_COLOR_GREEN "%d" ANSI_COLOR_RESET " passed, " ANSI_COLOR_RED "%d" ANSI_COLOR_RESET " failed.\n", total, successes, failures);
+    printf("Ran %d tests, " ANSI_COLOR_GREEN "%d" ANSI_COLOR_RESET
+           " passed, " ANSI_COLOR_RED "%d" ANSI_COLOR_RESET " failed.\n",
+           total, successes, failures);
   }
-
-  remove(tmp_file);
+  remove(result_file);
   return 0;
 }
